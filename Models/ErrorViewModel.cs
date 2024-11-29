f9c1151d2d7d23cddfdee5b923fa4d5135d945d7
risodkarshreyas <risodkarshreyas@gmail.com>
2024-11-29 18:44:48 +0000

    Updated index page

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PPP1CulturalCommitee.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
